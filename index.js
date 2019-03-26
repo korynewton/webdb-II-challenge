155a5e1a7f654e4cfd16a947b66f568c6991362f
korynewton <korynewtondev@gmail.com>
2019-03-26 12:26:37 -0700

    knex and sqlite3 dependencies brought in, server initialized, get request to db working on GET requests to '/' endpoint

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3'
+  }
+}
+
+const db = knex(knexConfig)
 
 const server = express();
 
@@ -7,6 +17,15 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', async (req, res) => {
+  try {
+    const dbres = await db('zoos')
+    res.status(200).json(dbres)
+  } catch {
+    res.status(500).json({ message: "error in retrieving items" })
+  }
+  
+})
 
 const port = 3300;
 server.listen(port, function() {
